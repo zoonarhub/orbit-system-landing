d2d86c730bbb68fdc3e859bb139d83e31ba05f5e
zoonarhub <zoonarhub@gmail.com>
2026-01-29 16:35:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [
+      'zoonar-site-landpage.ipyvqk.easypanel.host'
+    ]
+  }
+})
