ca89941363368efb1d2e860efbcb488f191c9a76
zoonarhub <zoonarhub@gmail.com>
2026-01-29 16:38:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   preview: {
     host: true,
-    port: 4173,
-    allowedHosts: [
-      'zoonar-site-landpage.ipyvqk.easypanel.host'
-    ]
+    allowedHosts: 'all'
   }
 })
