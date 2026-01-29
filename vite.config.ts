85136e7667e88538b716d7070638798206e902d4
zoonarhub <zoonarhub@gmail.com>
2026-01-29 19:08:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['zoonar.com.br']
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
